eb131e04f25b84c83cbce836f726b72d0adedd88
ViieeS <vies@list.ru>
2014-10-06 03:45:39 +0600

    Услуги, контент слайдера

--- www/index.php
+++ www/index.php
@@ -9,11 +9,11 @@ $template->set_vars('PAGE_TITLE','sc.ru');
 $template->set_vars('COPYRIGHT','sc.ru &copy; 2014');
 //$content = $template->get_file("pages/home.html", true);
 $menu = $template->get_file("parts/menu.html");
-$slider = $template->get_file("parts/slider.html");
+//$slider = $template->get_file("parts/slider.html");
 $hello = $template->get_file("parts/hello.html");
 $s_hello = $template->get_file("parts/prices_blocks.html");
 $template->set_vars('MENU',$menu);
-$template->set_vars('SLIDER',$slider,'home');
+//$template->set_vars('SLIDER',$slider,'home');
 $template->set_vars('HELLO',$s_hello,'services');
 $template->set_vars('HELLO',$hello,'home');
 
